65051a1162a5b7f611669103195f51268dfef302
Cristian Di Pietrantonio <cdipietrantonio@pawsey.org.au>
2023-08-08 16:02:04 +0800

    Adds new versions and variants.

--- var/spack/repos/builtin/packages/nektar/package.py
+++ var/spack/repos/builtin/packages/nektar/package.py
@@ -12,25 +12,29 @@ class Nektar(CMakePackage):
     homepage = "https://www.nektar.info/"
     url = "https://gitlab.nektar.info/nektar/nektar/-/archive/v4.4.1/nektar-v4.4.1.tar.bz2"
 
+    version('5.1.0', sha256='f5fdb729909e4dcd42cb071f06569634fa87fe90384ba0f2f857a9e0e56b6ac5')
+    version('5.0.3', sha256='1ef6f8f94f850ae78675bca3f752aa6c9f75401d1d6da4ec25df7fa795b860e9')
+    version('5.0.2', sha256='24af60a48dbdf0455149540b35a6a59acd636c47b3150b261899a1a1ca886c0b')
     version("5.0.0", sha256="5c594453fbfaa433f732a55405da9bba27d4a00c32d7b9d7515767925fb4a818")
+    
     version("4.4.1", sha256="71cfd93d848a751ae9ae5e5ba336cee4b4827d4abcd56f6b8dc5c460ed6b738c")
 
     variant("mpi", default=True, description="Builds with mpi support")
+    variant('avx2', default=True, description='Builds with simd avx2 support')
     variant("fftw", default=True, description="Builds with fftw support")
     variant("arpack", default=True, description="Builds with arpack support")
     variant("hdf5", default=True, description="Builds with hdf5 support")
     variant("scotch", default=False, description="Builds with scotch partitioning support")
+    variant('unit-tests', default=False, description='Builds unit tests')
+    variant('regression-tests', default=False, description='Builds regression tests')
+    variant('benchmarking-tests', default=False, description='Builds benchmark timing codes')
+    variant('python', default=False, description='Builds python bindings')
 
     depends_on("cmake@2.8.8:", type="build", when="~hdf5")
     depends_on("cmake@3.2:", type="build", when="+hdf5")
 
     depends_on("blas")
     depends_on("lapack")
-    depends_on(
-        "boost@1.56.0:"
-        "+iostreams+exception+filesystem+system+chrono+serialization"
-        "+atomic+regex+math+thread+container"
-    )
     depends_on("tinyxml", when="platform=darwin")
 
     depends_on("mpi", when="+mpi")
@@ -56,4 +60,10 @@ class Nektar(CMakePackage):
         args.append("-DNEKTAR_USE_HDF5=%s" % hasfeature("+hdf5"))
         args.append("-DNEKTAR_USE_SCOTCH=%s" % hasfeature("+scotch"))
         args.append("-DNEKTAR_USE_PETSC=OFF")
+        args.append('-DNEKTAR_ENABLE_SIMD_AVX2=%s' % hasfeature('+avx2'))
+        args.append('-DNEKTAR_BUILD_UNIT_TESTS=%s' % hasfeature('+unit-tests'))
+        args.append('-DNEKTAR_BUILD_TESTS=%s' % hasfeature('+regression-tests'))
+        args.append('-DNEKTAR_BUILD_TIMINGS=%s' % hasfeature('+benchmarking-tests'))
+        args.append('-DNEKTAR_BUILD_PYTHON=%s' % hasfeature('+python'))
+        
         return args
